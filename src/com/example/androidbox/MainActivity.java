254a0630c84af850d2ab88333b45fb22dd5af0b3
alex <alex-zhao76@hotmail.com>
2015-01-08 15:29:09 +0800

    add multiProcessService

--- src/com/example/androidbox/MainActivity.java
+++ src/com/example/androidbox/MainActivity.java
@@ -4,18 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
+	
+	String tag = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +34,7 @@ public class MainActivity extends ActionBarActivity {
 //                    .commit();
 //        }
         
-		Button getSystem = (Button) findViewById(R.id.getSystem);
-		Button accessInternet = (Button) findViewById(R.id.accessInternet);
-		
+		Button getSystem = (Button) findViewById(R.id.getSystem);	
 		getSystem.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -42,18 +46,37 @@ public class MainActivity extends ActionBarActivity {
 			}
 		});
 		
+		Button accessInternet = (Button) findViewById(R.id.accessInternet);
 		accessInternet.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
+				
+		        SharedPreferences sPreferences = getSharedPreferences("boxtestSetting", Build.VERSION.SDK_INT > 9 ? 4 : Context.MODE_PRIVATE);
+		        String value = sPreferences.getString("testkey1", "-1");
+		        Log.w(tag, "In MainActivity, read value=: " + value);
+				SharedPreferences.Editor editor = sPreferences.edit(); 
+				editor.putString("testkey1", String.valueOf(Long.parseLong(value) + 11L));  
+				editor.commit(); 
+				Log.w(tag, "In MainActivity, read value + 11 =: " + sPreferences.getString("testkey1", "-1"));
+				
+				
 				// TODO Auto-generated method stub
 				Intent intent = new Intent();
 				intent.setClass(MainActivity.this, InternetUtilActivity.class);
 				MainActivity.this.startActivity(intent);
 			}
 		});
+		
+		Button multiProcess = (Button) this.findViewById(R.id.multiProcess);
+		multiProcess.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {			
+	        Log.w(tag,"multiprocess on create() in MainActivity.");
+	        MainActivity.this.startService(new Intent(MainActivity.this,MultiProcessService.class));
+		}
+	});	
+		
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -94,5 +117,12 @@ public class MainActivity extends ActionBarActivity {
 	private void showDialog(String str) {
 		new AlertDialog.Builder(this).setMessage(str).show();
 	}
-
+	
+	/*
+	 *
+long currentTime = System.currentTimeMillis();
+SimpleDateFormat formatter = new SimpleDateFormat("yyyy年-MM月dd日-HH时mm分ss秒");
+Date date = new Date(currentTime);
+System.out.println(formatter.format(date));
+*/
 }
---
+++ src/com/example/androidbox/MultiProcessService.java
@@ -0,0 +1,45 @@
+package com.example.androidbox;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MultiProcessService extends Service {
+
+	String tag = "MultiProcess";
+    @Override
+    public void onCreate() {
+        Log.w(tag,"MultiProcessService is oncreate");
+        
+        SharedPreferences sPreferences = getSharedPreferences("boxtestSetting", Build.VERSION.SDK_INT > 9 ? 4 : Context.MODE_PRIVATE);
+        String value = sPreferences.getString("testkey1", "-1");
+        Log.w(tag, "In MultiProcessService onCreate, read value=: " + value);
+        
+		SharedPreferences.Editor editor = sPreferences.edit(); 
+		editor.putString("testkey1", String.valueOf(Long.parseLong(value) + 11L));  
+		editor.commit(); 
+		Log.w(tag, "In MultiProcessService onCreate, read value + 11 =: " + sPreferences.getString("testkey1", "-1"));
+    }
+    
+    
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+    	Log.w(tag, "In MultiProcessService onStartCommand");
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+    	Log.w(tag,"MultiProcess OnDestory");
+    }
+    
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
